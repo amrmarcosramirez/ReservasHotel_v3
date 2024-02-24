69e02f67cbc2c3125caaf8e90461e204fa85f6dd
amrmarcosramirez <150553422+amrmarcosramirez@users.noreply.github.com>
2024-02-24 21:49:38 +0100

    Create Doble.java
    Se crea la clase Doble

---
+++ src/main/java/org/iesalandalus/programacion/reservashotel/modelo/dominio/Doble.java
@@ -0,0 +1,69 @@
+package org.iesalandalus.programacion.reservashotel.modelo.dominio;
+
+public class Doble extends Habitacion{
+
+    // Atributos
+    private static final int NUM_MAXIMO_PERSONAS=2;
+    static final int MIN_NUM_CAMAS_INDIVIDUALES=0;
+    static final int MAX_NUM_CAMAS_INDIVIDUALES=2;
+    static final int MIN_NUM_CAMAS_DOBLES=0;
+    static final int MAX_NUM_CAMAS_DOBLES=1;
+    private int numCamasIndividuales;
+    private int numCamasDobles;
+
+    //Constructores
+    public Doble(int planta, int puerta, double precio, int numCamasIndividuales,
+                 int numCamasDobles){
+        super(planta, puerta, precio);
+        setNumCamasIndividuales(numCamasIndividuales);
+        setNumCamasDobles(numCamasDobles);
+    }
+
+    public Doble(Doble habitacionDoble){
+        super(habitacionDoble.getPlanta(), habitacionDoble.getPuerta(),
+                habitacionDoble.getPrecio());
+        setNumCamasIndividuales(habitacionDoble.getNumCamasIndividuales());
+        setNumCamasDobles(habitacionDoble.getNumCamasDobles());
+    }
+
+    public int getNumCamasIndividuales() {
+        return numCamasIndividuales;
+    }
+
+    public void setNumCamasIndividuales(int numCamasIndividuales) {
+        this.numCamasIndividuales = numCamasIndividuales;
+    }
+
+    public int getNumCamasDobles() {
+        return numCamasDobles;
+    }
+
+    public void setNumCamasDobles(int numCamasDobles) {
+        this.numCamasDobles = numCamasDobles;
+    }
+
+    private void validaNumCamas(){
+        if (getNumCamasIndividuales() < MIN_NUM_CAMAS_INDIVIDUALES ||
+            getNumCamasIndividuales() > MAX_NUM_CAMAS_INDIVIDUALES) {
+            throw new IllegalArgumentException("ERROR: El número de camas individuales no puede ser " +
+                    "inferior a " + MIN_NUM_CAMAS_INDIVIDUALES + " ni mayor que " +
+                    MAX_NUM_CAMAS_INDIVIDUALES + ".");
+        }
+        if (getNumCamasDobles() < MIN_NUM_CAMAS_DOBLES ||
+                getNumCamasDobles() > MAX_NUM_CAMAS_DOBLES) {
+            throw new IllegalArgumentException("ERROR: El número de camas dobles no puede ser " +
+                    "inferior a " + MIN_NUM_CAMAS_DOBLES + " ni mayor que " +
+                    MAX_NUM_CAMAS_DOBLES + ".");
+        }
+    }
+
+    @Override
+    public int getNumeroMaximoPersonas() {
+        return NUM_MAXIMO_PERSONAS;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("capacidad=%d personas", this.getNumeroMaximoPersonas());
+    }
+}
