514a7dbab2a63eb951942f9b164e80180c5839e7
amrmarcosramirez <150553422+amrmarcosramirez@users.noreply.github.com>
2024-02-24 20:56:44 +0100

    Create Simple.java
    Se crea la clase Simple

---
+++ src/main/java/org/iesalandalus/programacion/reservashotel/modelo/dominio/Simple.java
@@ -0,0 +1,29 @@
+package org.iesalandalus.programacion.reservashotel.modelo.dominio;
+
+import java.util.Objects;
+
+public class Simple extends Habitacion{
+
+    // Atributos
+    private static final int NUM_MAXIMO_PERSONAS=1;
+
+    //Constructores
+    public Simple(int planta, int puerta, double precio){
+        super(planta, puerta, precio);
+    }
+
+    public Simple(Simple habitacionSimple){
+        super(habitacionSimple.getPlanta(), habitacionSimple.getPuerta(),
+                habitacionSimple.getPrecio());
+    }
+
+    @Override
+    public int getNumeroMaximoPersonas() {
+        return NUM_MAXIMO_PERSONAS;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("capacidad=%d personas", this.getNumeroMaximoPersonas());
+    }
+}
