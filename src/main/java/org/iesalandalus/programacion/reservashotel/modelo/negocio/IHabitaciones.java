15e75bde560ebd71cd16be400147aa8de33447b7
amrmarcosramirez <150553422+amrmarcosramirez@users.noreply.github.com>
2024-02-25 14:16:09 +0100

    Create IHabitaciones.java
    Se crea la interface

---
+++ src/main/java/org/iesalandalus/programacion/reservashotel/modelo/negocio/IHabitaciones.java
@@ -0,0 +1,15 @@
+package org.iesalandalus.programacion.reservashotel.modelo.negocio;
+
+import org.iesalandalus.programacion.reservashotel.modelo.dominio.*;
+
+import javax.naming.OperationNotSupportedException;
+import java.util.List;
+
+public interface IHabitaciones {
+    List<Habitacion> get();
+    List<Habitacion> get(TipoHabitacion tipoHabitacion);
+    int getTamano();
+    void insertar(Habitacion habitacion) throws OperationNotSupportedException;
+    Habitacion buscar(Habitacion habitacion);
+    void borrar(Habitacion habitacion) throws OperationNotSupportedException;
+}
